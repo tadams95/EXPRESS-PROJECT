5c457eed1546698b06a7d44868e335bcf71cd643
tadams95 <tyrelleadams28@gmail.com>
2022-11-28 13:40:02 -0800

    Sending Files with res.send

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 function getMessage(req, res) {
-  res.send("This is the messages route.");
+  res.sendFile(path.join(__dirname, "..", "public", "skimountain.jpg"));
+  //   res.send("This is the messages route.");
 }
 
 function postMessage(req, res) {
