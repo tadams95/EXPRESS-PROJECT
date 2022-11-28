d414e01accb07ee7128b201e08767638b80ed5b0
tadams95 <tyrelleadams28@gmail.com>
2022-11-28 14:19:02 -0800

    Add handlebars templating engine

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const messagesRouter = require("./routes/messages.router");
 
 const app = express();
 
+//load handlebars and load templates
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
 const PORT = 3000;
 
 app.use((req, res, next) => {
@@ -20,6 +24,13 @@ app.use((req, res, next) => {
 app.use("/site", express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.render("index", {
+      title: "My friends are very clever",
+      caption: "Let\'s go to the snow!"
+    })
+});
 
 app.use("/friends", friendsRouter);
 app.use("/messages", messagesRouter);
